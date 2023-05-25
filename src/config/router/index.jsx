14f885a7470a6738431ffbed410fecc15b9f874b
RezaldhoArmadhani <rezaldhoarmadhani11@gmail.com>
2023-05-25 20:25:44 +0700

    fix name season two

--- src/config/router/index.jsx
+++ src/config/router/index.jsx
@@ -10,7 +10,7 @@ import Checkout from "../../pages/Checkout/Checkout";
 import Test from "../../pages/Test";
 // import CreateProduct from "../../components/Modal/CreateProduct";
 // import UpdateProduct from "../../components/Modal/UpdateProduct";
-import MyOrder from "../../pages/Profile/seller/MyOrder";
+import MyOrder from "../../pages/Profile/seller/myOrder";
 import MyProfile from "../../pages/Profile/customer/index";
 
 const Router = () => {
